8b0d3492cca810122ce933c1d2fdf7931173ab3e
catbaron0 <catbaron@live.cn>
2019-05-16 22:47:10 +0800

    fit for latest version of EFB

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         "Topic :: Utilities"
     ],
     install_requires=[
-        "ehforwarderbot>=2.0.0b5",
+        "ehforwarderbot>=2.0.0b15",
         "python-telegram-bot>=10.0.0<12.0.0",
         "python-magic",
         "peewee",
